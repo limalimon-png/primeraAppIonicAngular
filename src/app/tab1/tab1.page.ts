5be50b8d326a0c47fdaebe58994502549a2e80fc
guillermo <gmartinez@sdi.es>
2022-01-12 10:11:53 +0100

    fin de la seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
+
+  ngOnInit() {
+    // si ponemos debugger se para en ese punto la aplicacion desde el navegador
+    
+    let numero=10;
+    numero =20;
+    numero +=10;
+    numero =0;
+    alert(numero);
+    console.log("hola mundo");
+  }
 
 }
